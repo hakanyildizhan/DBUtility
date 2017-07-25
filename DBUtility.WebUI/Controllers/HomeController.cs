3e00d9750f05b14af33d0a4afc08faea323b98d5
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2017-07-26 02:53:25 +0300

    initial spa scaffold

--- DBUtility.WebUI/Controllers/HomeController.cs
+++ DBUtility.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBUtility.WebUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,10 +11,43 @@ namespace DBUtility.WebUI.Controllers
     {
         private DBUtilityContext db = new DBUtilityContext();
 
+        [HttpGet]
         public ActionResult Index()
         {
+            FadingBodyView model = new FadingBodyView()
+            {
+                Title = "_SelectFile",
+                CanGoBack = false,
+                CanGoNext = true
+            };
 
-            return View();
+            return View(model);
+        }
+
+        [HttpPost]
+        public PartialViewResult Next(string currentView, string file)
+        {
+            string partialViewToRender = string.Empty;
+            bool canGoBack = true;
+            bool canGoNext = true;
+
+            switch (currentView)
+            {
+                case "_SelectFile":
+                    partialViewToRender = "_SelectApplication";
+                    break;
+                default:
+                    break;
+            }
+
+            FadingBodyView model = new FadingBodyView()
+            {
+                Title = partialViewToRender,
+                CanGoBack = canGoBack,
+                CanGoNext = canGoNext
+            };
+
+            return PartialView(partialViewToRender, model);
         }
     }
 }
---
+++ DBUtility.WebUI/Scripts/site.js
@@ -0,0 +1,25 @@
+﻿function onFormLoad() {
+    alert($("#back").attr("disabled"));
+}
+
+function nextForm() {
+    $.ajax({
+        url: "/Home/Next",
+        type: "post",
+        data: $("form").serialize(), //if you need to post Model data, use this
+        success: function (result) {
+            $("#partial").html(result);
+        }
+    });
+}
+
+function previousForm() {
+    $.ajax({
+        url: "/Home/Back",
+        type: "post",
+        data: $("form").serialize(), //if you need to post Model data, use this
+        success: function (result) {
+            $("#partial").html(result);
+        }
+    });
+}
