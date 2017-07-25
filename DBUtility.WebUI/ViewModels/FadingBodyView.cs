619328a9b924e9b52fe6909edef2c21aa20e2ab6
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2017-07-25 19:57:06 +0300

    spa initial

---
+++ DBUtility.WebUI/ViewModels/FadingBodyView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBUtility.WebUI.ViewModels
+{
+    public class FadingBodyView
+    {
+        public string Title { get; set; }
+        public bool CanGoBack { get; set; }
+        public bool CanGoNext { get; set; }
+    }
+}
